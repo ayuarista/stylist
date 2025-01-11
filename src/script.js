c6a345da16e7a4e4ec3ebdfcd5a0158551c25304
ayuarista <aristaadewi8@gmail.com>
2025-01-11 15:31:37 +0800

    add best seller

--- src/script.js
+++ src/script.js
@@ -1,41 +1,97 @@
-function productData(){
+function productNewArrival() {
+  return {
+    products: [
+      {
+        id: 1,
+        image: "./assets/new arrival/new1.png",
+        name: "T-shirt with Tape Details",
+        rating: 4.5,
+        price: "Rp. 1.249.000",
+      },
+      {
+        id: 2,
+        image: "./assets/new arrival/new2.png",
+        name: "F.C. Barcelona Academy ",
+        rating: 4.9,
+        price: "Rp. 949.000",
+      },
+      {
+        id: 3,
+        image: "./assets/new arrival/new3.png",
+        name: "Nike Sportswear",
+        rating: 4.3,
+        price: "Rp. 949.000",
+      },
+      {
+        id: 4,
+        image: "./assets/new arrival/new4.png",
+        name: "Kobe",
+        rating: 4,
+        price: "Rp. 849.000",
+      },
+      {
+        id: 5,
+        image: "./assets/new arrival/new5.png",
+        name: "Vertical Striped Shirt",
+        rating: 5,
+        price: "Rp. 930.000",
+      },
+      {
+        id: 6,
+        image: "./assets/new arrival/new6.png",
+        name: "Courage Graphic T-shirt",
+        rating: 4.8,
+        price: "Rp. 1.149.000",
+      },
+      {
+        id: 7,
+        image: "./assets/new arrival/new7.png",
+        name: "Nike Air Zoom Pegasus",
+        rating: 5,
+        price: "Rp. 1.299.000",
+      },
+      {
+        id: 8,
+        image: "./assets/new arrival/new8.png",
+        name: "Nike Sportswear",
+        rating: 4.5,
+        price: "Rp. 849.000",
+      },
+    ],
+  };
+}
+
+function productBestSeller(){
     return{
-        products:[
+        products: [
             {
                 id: 1,
-                image: "./assets/new arrival/new1.png",
-                name: "T-shirt with Tape Details",
-                rating: 4.5,
-                price: "Rp. 1.249.000",
+                image: "./assets/best seller/best1.png",
+                name: "Vertical Striped Shirt",
+                rating: 4.9,
+                price: "Rp. 849.000",
             },
             {
                 id: 2,
-                image: "./assets/new arrival/new2.png",
-                name: "F.C. Barcelona Academy ",
-                rating: 4.9,
-                price: "Rp. 949.000"
+                image: "./assets/best seller/best2.png",
+                name: "Club Fleece",
+                rating: 4.5,
+                price: "Rp. 450.000",
             },
             {
                 id: 3,
-                image: "./assets/new arrival/new3.png",
-                name: "Nike Sportswear",
-                rating: 4.3,
-                price: "Rp. 949.000"
+                image: "./assets/best seller/best3.png",
+                name: "Loose Fit Bermuda Shorts",
+                rating: 5,
+                price: "Rp. 549.000",
             },
             {
                 id: 4,
-                image: "./assets/new arrival/new4.png",
-                name: "Kobe",
+                image: "./assets/hero/hero2.png",
+                name: "Older Kids",
                 rating: 4,
-                price: "Rp. 849.000"
-            },
-            {
-                id: 5,
-                image: "./assets/new arrival/new4.png",
-                name: "Kobe",
-                rating: 4,
-                price: "Rp. 849.000"
-            },
+                price: "Rp. 649.000",
+            }
         ]
     }
 }
