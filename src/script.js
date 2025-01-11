b7fbf5cd509bf4cd0fb94c3b42245c5a30104d27
ayuarista <aristaadewi8@gmail.com>
2025-01-11 14:45:58 +0800

    add alpine.js

---
+++ src/script.js
@@ -0,0 +1,41 @@
+function productData(){
+    return{
+        products:[
+            {
+                id: 1,
+                image: "./assets/new arrival/new1.png",
+                name: "T-shirt with Tape Details",
+                rating: 4.5,
+                price: "Rp. 1.249.000",
+            },
+            {
+                id: 2,
+                image: "./assets/new arrival/new2.png",
+                name: "F.C. Barcelona Academy ",
+                rating: 4.9,
+                price: "Rp. 949.000"
+            },
+            {
+                id: 3,
+                image: "./assets/new arrival/new3.png",
+                name: "Nike Sportswear",
+                rating: 4.3,
+                price: "Rp. 949.000"
+            },
+            {
+                id: 4,
+                image: "./assets/new arrival/new4.png",
+                name: "Kobe",
+                rating: 4,
+                price: "Rp. 849.000"
+            },
+            {
+                id: 5,
+                image: "./assets/new arrival/new4.png",
+                name: "Kobe",
+                rating: 4,
+                price: "Rp. 849.000"
+            },
+        ]
+    }
+}
