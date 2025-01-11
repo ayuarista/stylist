3e2899a5310445fd191de1beddeb3a801c6e5ada
ayuarista <aristaadewi8@gmail.com>
2025-01-11 10:55:43 +0800

    add header and hero

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        dela: "Dela Gothic One, serif" ,
+        syne: "Syne, serif"
+      }
+    },
+  },
+  plugins: [],
+}
